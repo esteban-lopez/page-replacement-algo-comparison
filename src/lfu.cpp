45c1b1f627f0d9f6b2c61818f41a757f35499e8e
yongrae <neurosilver@gmail.com>
2017-01-29 14:13:43 +0900

    add lfu mfu

---
+++ src/lfu.cpp
@@ -0,0 +1,39 @@
+#include "pr-sim.h"
+#include "gen-random-seq.h"
+#include <list>
+#include <map>
+#include <algorithm>
+
+int LFU::count_page_fault(struct context *ctx) {
+    std::vector<int> ref_seqeunce(ctx->nref);
+    gen_ref_seq(ctx->npage_min, ctx->npage_max, ref_seqeunce);
+    
+    std::list<int> loaded_pages; // Initially, It contains 0 because of demand paging
+    std::map<int, int> ref_counting_table;
+    int nfault = 0;
+
+    for (int i = 0; i < ctx->nref; i++) {
+        int ref_page = ref_seqeunce[i];
+        auto has_page =
+            !(std::find(loaded_pages.begin(), loaded_pages.end(), ref_page) == loaded_pages.end());
+        if (has_page == false) {
+            ++nfault;
+            if (loaded_pages.size() >= ctx->available_frames) {
+                auto lfu_page = loaded_pages.begin();
+                // find the least freqeuntly used page
+                for (auto &p : loaded_pages) {
+                    if (ref_counting_table[*lfu_page] > ref_counting_table[p]) {
+                        *lfu_page = p;
+                    }
+                }
+                //auto victim = std::find(loaded_pages.begin(), loaded_pages.end(), lfu_page);
+                loaded_pages.erase(lfu_page);
+            }
+            loaded_pages.push_back(ref_page);
+        } else {
+            
+        }
+        ref_counting_table[ref_page]++;
+    }
+    return nfault;
+}
